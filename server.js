af99796d94cceebd74284516a28de58e1cbfdce4
Kit Hoang <khangkhangtg@gmail.com>
2020-04-26 22:32:00 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+
 const app = express();
+
+// parse application/json
+app.use(bodyParser.json());
 
 const mockUserData = [
 	{name: 'Khang'},
 	{name: 'Quang'}
 ];
+
+app.post('/login', (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	const mockUsername = 'admin';
+	const mockPassword = '1234';
+
+	if(username === mockUsername && password === mockPassword) {
+		res.json({
+			success: true,
+			message: 'password and username match',
+			token: 'encrypted token goes here'
+		});
+	}else {
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		});
+	}
+});
 
 app.get('/users', (req, res) => {
 	res.json({
