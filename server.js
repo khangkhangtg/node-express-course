8ce264ca2e1c5f6553bf7713b16404dcad95ff8e
Kit Hoang <khangkhangtg@gmail.com>
2020-04-26 16:37:04 +0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+	{name: 'Khang'},
+	{name: 'Quang'}
+];
+
+app.get('/users', (req, res) => {
+	res.json({
+		success: true,
+		message: 'successfully got users, nice!',
+		users: mockUserData
+	})
+})
+
 app.listen(8000, () => {
 	console.log("Server is running");
 });
